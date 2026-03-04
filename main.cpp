e1c1e7602caf524a619ec5b73ca35cedffd5e18f
Igor Chumikov <2vbirf@gmail.com>
2026-03-04 17:19:11 +0000

    add div and expo

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 // -1 comment
 // test
+// correci
 int main ()
 {
 
@@ -8,7 +9,7 @@ int main ()
 	a = 10;
 
 	int b;
-	b = 15;
+	b = 3;
 
 	int c;
 	c = a + b;
@@ -21,5 +22,40 @@ int main ()
 	int e;
 	e = a * b;
 	std::cout << a << " * " << b << " = " << e << '\n';
+// add div and expo
+	
+	int f;
+	/*
+	 если b не равно 0 
+	 	с = a / b
+	иначе
+		вывести ошибку
+	 */
+	f = a / b;
+	if (b != 0)
+	{
+		f = a / b;
+	}
+	else
+	{
+		std::cout << "Error!\n";
+	}
+	std::cout << a << " / " << b << " = " << f << '\n';
 
+
+	/*for (действие до начала цикла;
+             проверка условия продолжения цикла;
+	     действие после каждой итерации цикла)
+	     {
+	     	тело цикла
+	     }
+	 */
+	int g = 1;
+	for (int i = 0; i < b; ++i)
+	{
+	g = g * a;
+	}
+	std::cout << a << " ^ " << b << " = " << g << '\n';
+
+	
 }
