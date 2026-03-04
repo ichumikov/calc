8a3884ec3e2e8508d07f6d2940a525e4703e0a6f
Igor Chumikov <2vbirf@gmail.com>
2026-03-04 15:02:23 +0000

    First status

---
+++ main.cpp
@@ -0,0 +1,24 @@
+nclude <iostream>
+
+int main ()
+{
+
+	int a;
+	a = 10;
+
+	int b;
+	b = 15;
+
+	int c;
+	c = a + b;
+	std::cout << a << " + " << b << " = " << c << '\n';
+
+	int d;
+	d = a - b;
+	std::cout << a << " - " << b << " = " << d << '\n';
+
+	int e;
+	e = a * b;
+	std::cout << a << " * " << b << " = " << e << '\n';
+
+}
